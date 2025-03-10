85005a4f9ebe765b9504e6c86c286a8e5e652b82
Kim <lademokim@hotmail.com>
2025-03-10 20:29:01 +0100

    Change to Northflank postgres

--- database/db.js
+++ database/db.js
@@ -1,14 +1,17 @@
 const postgres = require("postgres");
 require("dotenv").config();
 
-const sql = postgres({
-  host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PORT || 5432,
-  ssl:
-    process.env.DATABASE_SSL === "true" ? { rejectUnauthorized: false } : false,
+const sql = postgres(process.env.DATABASE_URL, {
+  ssl: { rejectUnauthorized: false },
 });
+
+(async () => {
+  try {
+    const result = await sql`SELECT NOW();`;
+    console.log("Database connected:", result);
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+  }
+})();
 
 module.exports = sql;
